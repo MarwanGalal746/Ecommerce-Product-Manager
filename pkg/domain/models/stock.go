6f41e4cc03ea6a274768a5656827ea1bf84da3fd
Marwan Galal <marawangalal84@gmail.com>
2022-09-09 09:13:27 +0200

    database update

--- pkg/domain/models/stock.go
+++ pkg/domain/models/stock.go
@@ -1,7 +1,9 @@
 package models
 
 type Stocks struct {
-	ProductId int `json:"productId" gorm:"not null;primaryKey;not null;index:idx_productCountry"`
-	CountryId int `json:"countryId" gorm:"not null;primaryKey;not null;index:idx_productCountry"`
-	Value     int `json:"value" gorm:"not null"`
+	ProductId  int     `json:"productId" gorm:"<-;type:int;not null;index:idx_productCountry"`
+	ProductObj Product `json:"productObj" gorm:"foreignKey:ProductId;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	CountryId  int     `json:"countryId" gorm:"<-;type:int;not null;index:idx_productCountry"`
+	CountryObj Country `json:"countryObj" gorm:"foreignKey:CountryId;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	Value      int     `json:"value" gorm:"not null"`
 }
